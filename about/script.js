3bcaebfa56586eb4a5d2488b2c518d783768a024
Alawiye Muritala <naijabayz@gmail.com>
2023-04-02 14:14:53 +0100

    complete the about page and started the contact page

--- about/script.js
+++ about/script.js
@@ -1,6 +1,7 @@
 // mission and vision
 const parentEl = document.querySelector(".mission-vision");
-// const indicators = document.querySelector("indicators").children;
+const indicator1 = document.querySelector(".indicator1");
+const indicator2 = document.querySelector(".indicator2");
 
 function swithchBtwnMissionVision() {
   setInterval(() => {
@@ -9,3 +10,11 @@ function swithchBtwnMissionVision() {
 }
 
 swithchBtwnMissionVision();
+
+indicator1.addEventListener("click", () => {
+  parentEl.classList.remove("active");
+});
+
+indicator2.addEventListener("click", () => {
+  parentEl.classList.add("active");
+});
